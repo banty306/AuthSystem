979f75c9f8d9da9708e17bc47f64e1c3753ed324
B. K. Manjhi <45758942+banty306@users.noreply.github.com>
2021-10-15 21:38:48 +0530

    final

--- server.js
+++ server.js
@@ -1,89 +1,98 @@
-const express = require("express")
-const bcrypt = require("bcrypt")
-const { validationResult } = require('express-validator')
-const { loginDetailsChecker,checkUserDetails } = require("./middleware/validation")
-const cors = require("cors")
-const fs = require("fs")
-const PORT = 5000
-const SALT = 10
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
-
-// POST sign_up ,must not contain username or email
-app.post('/sign_up',checkUserDetails,async (req,res)=>{
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        console.log(errors.array())
-        return res.status(400).json({success:false})
-    }
-    const { name, email, password } = req.body
-    try{
-        const hashedPwd = bcrypt.hashSync(password,SALT)
-        let obj = {
-            name,
-            email,
-            password:hashedPwd
-        }
-        let data = JSON.stringify(obj)
-
-        fs.writeFile('./db.JSON',data,(err)=>{
-            if(err)
-                return res.status(400).json({success:false})
-            return res.status(201).json({success:true})
-        })
-    }catch(err){
-        console.error('An error occured',err)
-        return res.status(400).json({success:false})
-    }
-})
-
-// POST sign_in
-app.post('/sign_in',loginDetailsChecker,async (req,res)=>{
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        console.log(errors.array())
-        return res.status(400).json({success:false})
-    }
-    const { email, password } = req.body
-    try{
-        fs.readFile('./db.JSON',(err,data)=>{
-            if(err)
-                return res.status(400).json({success:false})
-            const dataArr = JSON.parse(data)
-            const entry = dataArr.filter(element => element.email === email)
-            console.log(`entry`, entry)
-            if(!entry[0])
-                return res.status(400).json({success:false})
-            else{
-                let bool = bcrypt.compareSync(password,entry[0].password)
-                if(!bool)
-                    return res.status(400).json({success:false})
-                return res.status(200).json({success:true})
-            }
-        })  
-    }catch(err){
-        console.error('An error occured',err)
-        return res.status(400).json({success:false})
-    }
-})
-
-// POST clean {sucess:true}
-app.post('/clean',async (req,res)=>{
-    try{
-        fs.readFile('./db.JSON',(err,data)=>{
-            const dataObj = JSON.parse(data)
-            delete dataObj
-        })
-    }catch(err){
-        console.error('An error occured',err)
-        return res.json({success:false})
-    }
-})
-
-app.listen(PORT,()=>{
-    console.log(`Running on PORT: ${PORT}`)
+const express = require("express") 
+const bcrypt = require("bcrypt") 
+// const cors = require("cors") 
+const fs = require("fs") 
+const PORT = 5000 
+const SALT = 10 
+const app = express() 
+ 
+// app.use(cors()) 
+app.use(express.json()) 
+app.use(express.urlencoded({extended:true})) 
+ 
+ 
+// POST sign_up ,must not contain username or email 
+app.post('/sign_up',async (req,res)=>{ 
+    const { name, email, password } = req.body 
+    try{ 
+        if(name==="" || name === null || name ===undefined) 
+            return res.status(400).json({success:false}) 
+        let emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ 
+        let passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/ 
+ 
+        let emailParts = email.split("@"); 
+        if((password.indexOf(emailParts[0]) > -1 || password.indexOf(emailParts[1].split(".")[0]) > -1) || password.indexOf(name) >-1) 
+            return res.status(400).json({success:false}) 
+        if(!emailRegex.test(email) || !passwordRegex.test(password)) 
+            return res.status(400).json({success:false}) 
+        const hashedPwd = bcrypt.hashSync(password,SALT) 
+        let obj = [{ 
+            name, 
+            email, 
+            password:hashedPwd 
+        }] 
+        let data = JSON.stringify(obj) 
+ 
+        console.log(`data`, data) 
+ 
+         
+        fs.writeFile('./db.JSON',data, {'flag':'a'},(err)=>{ 
+            if(err) 
+                return res.status(400).json({success:false}) 
+            return res.status(201).json({success:true}) 
+        }) 
+    }catch(err){ 
+        console.error('An error occured',err) 
+        return res.status(400).json({success:false}) 
+    } 
+}) 
+ 
+// POST sign_in 
+app.post('/sign_in',async (req,res)=>{ 
+    const { email, password } = req.body 
+    try{ 
+        let emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ 
+        let passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/ 
+        if(!emailRegex.test(email) || !passwordRegex.test(password)) 
+            return res.status(400).json({success:false}) 
+        let emailParts = email.split("@"); 
+        if(password.indexOf(emailParts[0]) > -1 || password.indexOf(emailParts[1]) > -1) 
+            return res.status(400).json({success:false}) 
+        fs.readFile('./db.JSON',(err,data)=>{ 
+            if(err) 
+                return res.status(400).json({success:false}) 
+            const dataArr = JSON.parse(data) 
+            const entry = dataArr.filter(element => element.email === email) 
+            console.log(`entry`, entry) 
+            if(!entry[0]) 
+                return res.status(400).json({success:false}) 
+            else{ 
+                let bool = bcrypt.compareSync(password,entry[0].password) 
+                if(!bool) 
+                    return res.status(400).json({success:false}) 
+                return res.status(200).json({success:true}) 
+            } 
+        })   
+    }catch(err){ 
+        console.error('An error occured',err) 
+        return res.status(400).json({success:false}) 
+    } 
+}) 
+ 
+// POST clean 
+app.post('/clean',async (req,res)=>{ 
+    try{ 
+        fs.writeFile('./db.JSON',"",(err,)=>{ 
+            if(err) 
+                console.log("Error in delete",err) 
+            return res.json({success:true}) 
+        }) 
+    }catch(err){ 
+        console.error('An error occured',err) 
+        return res.json({success:false}) 
+    } 
+}) 
+ 
+app.listen(PORT,()=>{ 
+    console.log(`Running on PORT: ${PORT}`) 
 })
